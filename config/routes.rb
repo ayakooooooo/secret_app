6107cc5e8ad324318b4032ce92c7ff0647d025fd
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-05 17:32:33 +0900

    ルートパスの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :clinical_records, only: :index
+  root to: 'clinical_records#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
